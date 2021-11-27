5f5a5becf5258d80c86098e8bc67533e9e75aa88
Jack Gordon <appjackstudio@gmail.com>
2021-11-27 13:45:24 -0600

    Send large emails as attachments instead of splitting them into multiple smaller messages

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,27 @@
-import { SMTPServer } from "smtp-server";
-import { simpleParser } from "mailparser";
-import fetch from "node-fetch";
+import { SMTPServer } from 'smtp-server';
+import { simpleParser } from 'mailparser';
+import fetch from 'node-fetch';
+import FormData from 'form-data';
 
-const MAX_MESSAGE_LEN = 2000
+const MAX_MESSAGE_LEN = 2_000
+const MAX_MESSAGE_ATTACHED_LEN = 8_000_000
+const FIELD_MAX_LEN = 50 // Length limit for things that should generally be short
+const EMAIL_TRUNCATED_MESSAGE = '...\nEmail was truncated, full email is attached'
 const WEBHOOK_URL = process.env.WEBHOOK_URL
-const TZ = process.env.TZ || 'UTC'
 
-async function sendMessage(content, username) {
+function streamToBuffer(stream) {
+    return new Promise((resolve, reject) => {
+        let chunks = []
+        stream.on('data', chunk => chunks.push(chunk))
+        stream.on('end', () => resolve(Buffer.concat(chunks)))
+        stream.on('error', reject)
+    })
+}
+
+async function sendMessageFull(content, username) {
+    console.log(content)
+    console.log(content.length)
+
     let response = await fetch(WEBHOOK_URL + '?wait=true', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -18,17 +33,46 @@ async function sendMessage(content, username) {
     }
 }
 
-async function onData(stream, session) {
-    let parsed = await simpleParser(stream, {})
+async function sendMessageTruncatedAttachment(content, username, email) {
+    let formData = new FormData()
+
+    let payload = JSON.stringify({
+        content, username,
+        attachments: [{
+            id: 0,
+            ephemeral: true
+        }]
+    })
+
+    formData.append('payload_json', payload, { contentType: 'application/json' })
+    formData.append('files[0]', email, { contentType: 'message/rfc822', filename: 'email.eml' })
+
+    let response = await fetch(WEBHOOK_URL + '?wait=true', {
+        method: 'POST',
+        body: formData
+    })
+
+    if (!response.ok) {
+        throw new Error(`Error: response ${response.status} (${response.statusText})\n${await response.buffer()}`)
+    }
+}
+
+async function onData(stream) {
+    let email = await streamToBuffer(stream)
+
+    if (email.length > MAX_MESSAGE_ATTACHED_LEN) {
+        throw new Error(`Email too large, max size is ${MAX_MESSAGE_ATTACHED_LEN} bytes`)
+    }
+
+    let parsed = await simpleParser(email, {})
     console.log(`New message from ${parsed.from.text} to ${parsed.to?.text} "${parsed.subject}"`)
 
+    console.log(parsed)
     let content = parsed.text.trim()
         .split('\n')
-        // Split lines longer than 2000 chars into multiple lines
-        .flatMap(line => line.match(new RegExp(`.{1,${MAX_MESSAGE_LEN - 5}}`)))
         .map(line => `> ${line || ''}\n`)
 
-    console.log("Forwarding message to discord webhook")
+    console.log('Forwarding message to discord webhook')
 
     let to = ''
     if (Array.isArray(parsed.to)) {
@@ -38,22 +82,22 @@ async function onData(stream, session) {
     }
 
     let message = `To: \`${to}\`\n` +
-        `Date: <t:${Math.floor(parsed.date.getTime() / 1000)}>\n` +
-        `Subject: **${parsed.subject}**\n`
-    let username = `Email from ${parsed.from.text}`
+        `Date: <t:${Math.floor(parsed.date?.getTime() / 1000)}>\n` +
+        `Subject: **${parsed.subject.trim(0, FIELD_MAX_LEN)}**\n`
+    let username = `Email from ${parsed.from.text.trim(0, FIELD_MAX_LEN)}`
 
-    // Split long emails into multiple messages
     for (let line of content) {
-        if (message.length + line.length > MAX_MESSAGE_LEN) {
-            await sendMessage(message, username)
-            console.log("sent partial message")
-            message = ''
+        if (message.length + line.length + EMAIL_TRUNCATED_MESSAGE.length > MAX_MESSAGE_LEN) {
+            message += EMAIL_TRUNCATED_MESSAGE
+            await sendMessageTruncatedAttachment(message, username, email)
+            console.log('Sent truncated message')
+            return
         }
         message += line
     }
-    await sendMessage(message, username)
 
-    console.log("Message forwarded successfully")
+    await sendMessageFull(message, username)
+    console.log('Sent full message')
 }
 
 const server = new SMTPServer({
@@ -68,14 +112,19 @@ const server = new SMTPServer({
     authOptional: true
 });
 
-server.on("error", err => {
-    console.error("Error %s", err.message);
+server.on('error', err => {
+    console.error('Error %s', err.message);
 });
 
 process.on('SIGTERM', () => {
-    console.log("Shutting Down...")
+    console.log('Shutting Down...')
     server.close()
 })
 
-console.log("Starting Server")
+process.on('SIGINT', () => {
+    console.log('Shutting Down...')
+    server.close()
+})
+
+console.log('Starting Server')
 server.listen(8025)
